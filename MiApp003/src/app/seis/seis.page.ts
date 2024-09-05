04b115ebc9aa8cc4b42a21b415edf1491872c65a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-05 11:12:33 -0400

    update

--- MiApp003/src/app/seis/seis.page.ts
+++ MiApp003/src/app/seis/seis.page.ts
@@ -9,9 +9,19 @@ export class SeisPage implements OnInit {
   nombre:string;
   apellido:string;
   mensaje : string = "Ud es un super estudiante 2024";
+
+  asignaturas = [
+    {nombre : "PGY4121", nota: 39.5},
+    {nombre : "mdy3121", nota: 50},
+    {nombre : "PGY1011", nota: 40},
+  ];
+
+
   constructor() { 
     this.nombre = "Juan";
     this.apellido="Fernandez";
+    console.log(this.asignaturas);
+    console.table(this.asignaturas);
   }
 
   ngOnInit() {
