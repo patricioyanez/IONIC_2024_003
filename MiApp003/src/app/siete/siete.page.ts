dbed1283b14a0cd1db85e91eb4701577728dbd2e
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-10 11:54:59 -0300

    update

--- MiApp003/src/app/siete/siete.page.ts
+++ MiApp003/src/app/siete/siete.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./siete.page.scss'],
 })
 export class SietePage implements OnInit {
-
-  constructor() { }
+  nombre = 'Ana';
+  apellido : string;
+  n1='';
+  n2='';
+  resultado : any;
+  constructor() { 
+    this.apellido = 'Perez';
+  }
 
   ngOnInit() {
   }
-
+  sumar()
+  {
+    this.resultado=this.n1+this.n2;
+  }
 }
