efac30765556f52fdac09f75f4b495f760bed4cb
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-12 10:46:37 -0300

    update

--- MiApp003/src/app/siete/siete.page.ts
+++ MiApp003/src/app/siete/siete.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-siete',
@@ -11,14 +12,33 @@ export class SietePage implements OnInit {
   n1='';
   n2='';
   resultado : any;
-  constructor() { 
+  constructor(private toastController:ToastController) { 
     this.apellido = 'Perez';
   }
 
   ngOnInit() {
   }
-  sumar()
+  async sumar()
   {
+    if(!this.n1 || !this.n2)
+    {
+      const toast = await this.toastController.create({
+        message : 'Debe ingresar un número válido',
+        duration: 3000,
+        position: 'top',
+        color   : 'danger'
+      });
+      await toast.present();
+      return;
+    }
+
     this.resultado=this.n1+this.n2;
+    const toast = await this.toastController.create({
+      message : 'El resultado es ' + this.resultado,
+      duration: 3000,
+      position: 'top',
+      color   : 'primary'
+    });
+    await toast.present();
   }
 }
